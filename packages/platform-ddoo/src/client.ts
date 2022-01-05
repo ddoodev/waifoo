9f5ac5c10214a5655af859b3532b899f208579ab
Кислый Кислюк <kislball@yahoo.com>
2022-01-05 14:53:07 +0300

    feat: allow to customize build pipeline for client

--- packages/platform-ddoo/src/client.ts
+++ packages/platform-ddoo/src/client.ts
@@ -1,15 +1,23 @@
 import { App, DescendantOfClass, DotenvConfig, resolve } from '@waifoo/core'
 import { ClientBuilder, createApp as createDdooApp, DefaultClientStack, Client } from 'discordoo'
 
-export interface DiscordooClientConfig {
+/**
+ * Configuration for {@link DiscordooClient}
+ */
+export interface DiscordooClientConfig<T extends DefaultClientStack = DefaultClientStack> {
+  /** Async function which returns token */
   token: () => Promise<string>
+  /** Customize build pipeline */
+  builder: (b: ClientBuilder<T>) => ClientBuilder<T>
 }
 
-export const defaultDiscordooClientConfig: DiscordooClientConfig = {
+/** Default {@link DiscordooClientConfig} */
+export const defaultDiscordooClientConfig: DiscordooClientConfig<DefaultClientStack> = {
   async token() {
     const dotenvCfg = resolve(DotenvConfig)
     return await dotenvCfg.get('DISCORD_TOKEN') ?? ''
-  }
+  },
+  builder: b => b
 }
 
 export abstract class DiscordooClient<T extends DefaultClientStack = DefaultClientStack> extends App {
@@ -18,17 +26,19 @@ export abstract class DiscordooClient<T extends DefaultClientStack = DefaultClie
 
   async load() {
     this.clientBuilder = createDdooApp<T>(await this.getToken())
-    this.client = this.clientBuilder.build()
+    this.client = this.customBuilder(this.clientBuilder).build()
     await this.client.start()
   }
 
   abstract getToken(): Promise<string>
+  abstract customBuilder(b: ClientBuilder<T>): ClientBuilder<T>
 }
 
 export const createDiscordooClient = 
-  <T extends DefaultClientStack = DefaultClientStack>(cfg: Partial<DiscordooClientConfig>): DescendantOfClass<DiscordooClient<T>> => {
+  <T extends DefaultClientStack = DefaultClientStack>(cfg: Partial<DiscordooClientConfig<T>>): DescendantOfClass<DiscordooClient<T>> => {
     const config = { ...defaultDiscordooClientConfig, ...cfg }
     return class extends DiscordooClient<T> {
       getToken = config.token
+      customBuilder = config.builder
     }
   }
