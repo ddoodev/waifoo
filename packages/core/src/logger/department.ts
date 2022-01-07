539c2010ad0c6e50c3bd472d71fe91d9d45016d4
Кислый Кислюк <kislball@yahoo.com>
2022-01-07 16:43:13 +0300

    feat(core): fallback to silent if department is empty string

--- packages/core/src/logger/department.ts
+++ packages/core/src/logger/department.ts
@@ -1,5 +1,7 @@
+import { Silent } from './silent'
+
 export const DepartmentNameKey = Symbol('DepartmentName')
 /** Sets department key for logger */
-export const Department = (name: string) => Reflect.metadata(DepartmentNameKey, name)
+export const Department = (name: string) => name.length === 0 ? Silent() : Reflect.metadata(DepartmentNameKey, name)
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const getDepartment = (t: Function): string => Reflect.getMetadata(DepartmentNameKey, t) ?? ''
